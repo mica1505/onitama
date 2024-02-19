20681c790eef09e4c61abcd02cf0c5d282f973ca
mica1505 <cameliamazouz1505@gmail.com>
2024-02-19 11:41:00 +0100

    Creation de classe Carte

--- CODE/Carte.py
+++ CODE/Carte.py
@@ -1,5 +1,18 @@
 class Carte:
 
-    def __init__(self,nom,mouvements):
+    def __init__(self,nom,mouvements,pioche):
         self.nom = nom
         self.mouvements = mouvements
+        self.pioche = pioche
+    '''
+    {"Dragon":[],"Lapin":[],"Tigre":[],"Elephant":[],"Cobra":[],"Boeuf":[],"Crabe":[],"Singe":[],
+    "Cheval":[],"Sanglier":[],"Oie":[],"Angille":[],"Grenouille":[],"Menthe":[],"Coque":[],"Grue":[]}
+    '''
+    def creerCarte(self,nom):
+        return Carte(nom,self.pioche.getMouvs(nom),self.pioche)
+    
+    def getMouvs(self):
+        return self.mouvements
+    
+
+    
--- CODE/Pioche.py
+++ CODE/Pioche.py
@@ -1,3 +1,13 @@
 class Pioche :
     def __init__(self):
-        self.cartes = {"Dragon":[],"Lapin":[],"Tigre":[],"Elephant":[],"Cobra":[],"Boeuf":[],"Crabe":[],"Singe":[]}
+        '''
+        
+        '''
+        self.cartes = {"Dragon":[],"Lapin":[],"Tigre":[],"Elephant":[],"Cobra":[],"Boeuf":[],"Crabe":[],"Singe":[],
+                       "Cheval":[],"Sanglier":[],"Oie":[],"Angille":[],"Grenouille":[],"Menthe":[],"Coque":[],"Grue":[]}
+        
+    def getMouvs(self,nom):
+        '''
+        
+        '''
+        return self.cartes[nom]
